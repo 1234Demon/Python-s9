0d450733546de8fa62382f6f3432268e42f5db6e
Nikolay <chort.krsk@rambler.ru>
2023-01-17 23:31:38 +0700

    calc done, but...

--- homework1/func.py
+++ homework1/func.py
@@ -1,15 +1,18 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 import datetime
 
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Hi {update.effective_user.first_name}!')
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'command for new entry line by line /phonewrite1 Surname Name Phone \ncommand for new entry in one term /phonewrite2 Surname Name Phone \ncommand for read book line by line /phonebook1 \ncommand for read book line by line /phonebook2 \nother commands /hi /time')
+async def helpbook(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'command for new entry line by line /phonewrite1 Surname Name Phone \ncommand for new entry in one term /phonewrite2 Surname Name Phone \ncommand for read book line by line /phonebook1 \ncommand for read book line by line /phonebook2')
 
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'For begin send /start')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
@@ -41,3 +44,59 @@ async def phoneb1(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def phoneb2(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_document(chat_id=update.effective_chat.id, document=open('book2.txt', 'rb'))
+
+async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    variant = query.data
+    await query.answer()
+    await query.edit_message_text(text=f'{variant}')
+
+async def start1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keybord = [
+        [
+            InlineKeyboardButton('Lets meet!', callback_data='Command /hi for you!'),
+            InlineKeyboardButton('Wanna work with phonebook?', callback_data='Command /helpbook for you!'),
+        ],
+        [
+            InlineKeyboardButton('What time is it?', callback_data='Command /time for you!'),
+            InlineKeyboardButton('Some calc?', callback_data='Command /calc (rational nmb) and /calc1 (complex nmb) in format "command" number1 number2 '),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keybord)
+    await update.message.reply_text('Choose operation', reply_markup=reply_markup)
+
+async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    items = msg.split()
+    x = float(items[1])
+    y = float(items[2])
+    keybord1 = [
+        [
+            InlineKeyboardButton('+', callback_data=f'{x} + {y} = {x+y}'),
+            InlineKeyboardButton('-', callback_data=f'{x} - {y} = {x-y}'),
+        ],
+        [
+            InlineKeyboardButton('*', callback_data=f'{x} * {y} = {x*y}'),
+            InlineKeyboardButton('/', callback_data=f'{x} / {y} = {x/y}'),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keybord1)
+    await update.message.reply_text('Choose operation', reply_markup=reply_markup)
+
+async def calc1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    items = msg.split()
+    x = complex(items[1])
+    y = complex(items[2])
+    keybord = [
+        [
+            InlineKeyboardButton('+', callback_data=f'{x} + {y} = {x+y}'),
+            InlineKeyboardButton('-', callback_data=f'{x} - {y} = {x-y}'),
+        ],
+        [
+            InlineKeyboardButton('*', callback_data=f'{x} * {y} = {x*y}'),
+            InlineKeyboardButton('/', callback_data=f'{x} / {y} = {x/y}'),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keybord)
+    await update.message.reply_text('Choose operation', reply_markup=reply_markup)
--- homework1/main.py
+++ homework1/main.py
@@ -1,17 +1,24 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from func import *
+import tokentb
 
-app = ApplicationBuilder().token("5886482543:AAEqCIL0_HRhYAB-gd8RIzuT1B7XQZShmUY").build()
+app = ApplicationBuilder().token(tokentb.tokentb()).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
-app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("helpbook", helpbook))
 app.add_handler(CommandHandler("phonewrite1", phonewr1))
 app.add_handler(CommandHandler("phonewrite2", phonewr2))
 app.add_handler(CommandHandler("phonebook1", phoneb1))
 app.add_handler(CommandHandler("phonebook2", phoneb2))
+app.add_handler(CommandHandler("start1", start1))
+app.add_handler(CallbackQueryHandler(buttons))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("calc", calc))
+app.add_handler(CommandHandler("calc1", calc1))
+
 
 print('server starst')
 
